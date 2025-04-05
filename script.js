c43fd675423f11cecdca619e11ee7602e4332e1d
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2025-04-05 16:26:42 +0200

    may have fixed the functional part of the light dark ,ode

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
   const changeColorButton = document.getElementById("change-color");
   const colored = document.getElementById("colored");
-  changeColorButton.addEventListener("click", function () {
+  const nodeList = document.querySelectorAll(".colored, .footer");
+  console.log(nodeList.length);
+  for (let i = 0; i < nodeList.length; i++) {
+    changeColorButton.addEventListener("click", function () {
+      let currentColor = nodeList[i].style.backgroundColor;
+      console.log("0.5 = " + currentColor);
+
+      if (currentColor === "rgb(0, 0, 0)") {
+        const whiteC = "#FFFFFF";
+        currentColor = whiteC;
+        nodeList[i].style.backgroundColor = currentColor;
+        console.log("2 = " + currentColor);
+      } else {
+        const blackC = "#000000";
+        currentColor = blackC;
+        nodeList[i].style.backgroundColor = currentColor;
+        console.log("1 = " + currentColor);
+      }
+    });
+  }
+});
+
+/* document.addEventListener("DOMContentLoaded", function () {
+  const changeColorButton = document.getElementById("change-color");
+  const colored = document.getElementById("colored");
+  
+  hangeColorButton.addEventListener("click", function () {
     let currentColor = colored.style.backgroundColor;
     console.log("0.5 = " + currentColor);
 
@@ -18,3 +44,4 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+ */
